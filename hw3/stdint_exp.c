6402292b3ec97af91e035a01f35c10dc9a8ec966
AlisonHuang777 <alison.94.08.17@gmail.com>
2023-12-29 19:29:26 +0800

    upload stdint_exp.c

---
+++ hw3/stdint_exp.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    printf(
+        "%lu %lu %lu %lu %lu %lu %lu %lu"
+        , sizeof(int8_t),  sizeof(int16_t),  sizeof(int32_t),  sizeof(int64_t)
+        , sizeof(uint8_t), sizeof(uint16_t), sizeof(uint32_t), sizeof(uint64_t)
+    );
+}
